68146a42faf6024569e4ea9ceabc26da09f51248
Casey <caseyclinga@gmail.com>
2019-06-28 14:33:37 -0700

    Added code for handelbars view engine

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 //Require packages
 var express = require("express")
-var exHndbrs = require("express-handlebars")
+var exphbs = require("express-handlebars")
 var mongoose = require("mongoose")
 var axios = require("axios")
 var cheerio = require("cheerio")
 
 //Import models
 var db = require("./models")
+
+var PORT = 3000;
+
+//Initialize express
+var app = express();
+
+//Initialize handlebars
+app.engine("handlebars", exphbs({
+    defaultLayout: "index" 
+    });
+);
+
+//Set view engine
+app.set("view engine", "handlebars")
+
+
+//Middleware
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
 
