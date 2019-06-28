fc91bee80baddbadfe119eeacb8b9217a574a79f
Casey <caseyclinga@gmail.com>
2019-06-28 14:27:38 -0700

    Required packages and models

---
+++ server.js
@@ -0,0 +1,10 @@
+//Require packages
+var express = require("express")
+var exHndbrs = require("express-handlebars")
+var mongoose = require("mongoose")
+var axios = require("axios")
+var cheerio = require("cheerio")
+
+//Import models
+var db = require("./models")
+
