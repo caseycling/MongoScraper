22f432175104e76cd199ae56ec685d24a5f0bb4b
Casey <caseyclinga@gmail.com>
2019-06-30 12:30:19 -0700

    Created note model with mongoose

--- models/Note.js
+++ models/Note.js
@@ -1,1 +1,14 @@
 var mongoose = require("mongoose");
+
+//Reference to schema constructor 
+var Schema = mongoose.Schema;
+
+//Create NoteSchema object
+var NoteSchema = new Schema({
+    body: String
+});
+
+//Save model to variable
+var Note = mongoose.model("Note", NoteSchema);
+
+module.exports = Note;
