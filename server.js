e9a9388e87c766732af88a968bac0818f20792f0
Casey <caseyclinga@gmail.com>
2019-06-30 14:13:17 -0700

    Created GET route for displaying scraped articles

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var app = express();
 //Initialize handlebars
 app.engine("handlebars", exphbs({
     defaultLayout: "index" 
-    });
+    })
 );
 
 //Set view engine
@@ -31,3 +31,44 @@ app.use(express.json());
 mongoose.connect("mongodb://localhost/scraper", { useNewUrlParser: true });
 
 //Routes
+
+//GET route for displaying unsaved articles in root route
+app.get("/", function(req, res) {
+    db.Article.find({ saved: false})
+    .then(function(unsavedArticle) {
+        res.json(unsavedArticle)
+    })
+    .catch(function(err) {
+        res.json(err)
+    })
+})
+
+//GET route for scrapping
+app.get("/scrape", function(req, res) {
+    //Grab body of html with axios
+    axios.get("http://www.nytimes.com").then(function(response) {
+        
+        var $ = cheerio.load(response.data)
+
+        $("div.css-qvz0vj ").each(function(i, element) {
+            //Empty object to save results
+            var result ={}
+
+            //Object properties with values scraped from website
+            result.title = $(this).children("h2.css-n2blzn").text()
+            result.summary = $(this).children("p").text()
+            result.href = $(this).find("a").attr("href")
+            result.saved = false;
+
+            //Create new article using result object
+            db.Article.create(result)
+            .then(function(dbArticle) {
+                //View result in console
+                console.log(dbArticle)
+            })
+            .catch(function(err) {
+                console.log(err)
+            })
+        })
+    })
+})
