21f16ef85a9807dcab5c41778a49c82ea2a55301
Casey <caseyclinga@gmail.com>
2019-06-30 12:37:46 -0700

    Created article model

--- models/Article.js
+++ models/Article.js
@@ -1,1 +1,30 @@
 var mongoose = require("mongoose");
+
+//Save reference to Schema constructor
+var Schema = mongoose.Schema;
+
+//Create ArticleSchema object
+var ArticleSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    href: {
+        type: String,
+        required: true
+    },
+    saved: {
+        type: Boolean,
+        required: true
+    },
+    note: {
+        type: Schema.Types.ObjectId,
+        ref: "Note"
+    }
+})
+
+//Save model to a variable
+var Article = mongoose.model("Article", ArticleSchema)
+
+//Export model
+module.exports = Article
